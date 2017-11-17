5e9820b256efc025b3e819d7db988f2c3ef69bd2
Guilherme Medeiros <gui.medeiros1@gmail.com>
2017-11-17 17:43:36 -0200

    heroku

--- adapt_recommender/settings.py
+++ adapt_recommender/settings.py
@@ -167,14 +167,14 @@ if DEBUG:
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    #
+    # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATIC_URL = '/static/'
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    #
+    # # Extra places for collectstatic to find static files.
+    # STATICFILES_DIRS = (
+    #     os.path.join(PROJECT_ROOT, 'static'),
+    # )
+    #
+    # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
