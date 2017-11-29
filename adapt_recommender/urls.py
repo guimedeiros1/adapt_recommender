b5eb854bbd55b78bf48008bda203e40ce1ec49ff
Guilherme Medeiros <gui.medeiros1@gmail.com>
2017-11-29 16:52:12 -0200

    google analytics and instructions

--- adapt_recommender/urls.py
+++ adapt_recommender/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^recommender/', include('recommender.urls')),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^$', TemplateView.as_view(template_name='instructions.html')),
 ]
 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
